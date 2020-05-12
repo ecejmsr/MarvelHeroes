144a1dbe4ee0d1b9be0539f5265024db70621701
unknown <ecejmsr@gmail.com>
2020-05-11 22:56:05 -0500

    Updates TS functions

--- app/src/main/java/com/zensolutions/marvelheroes/data/network/impl/MarvelHeroFetchImpl.kt
+++ app/src/main/java/com/zensolutions/marvelheroes/data/network/impl/MarvelHeroFetchImpl.kt
@@ -5,6 +5,7 @@ import com.zensolutions.marvelheroes.data.model.networkModel.ServiceResult
 import com.zensolutions.marvelheroes.data.network.MarvelRetrofitService
 import com.zensolutions.marvelheroes.data.network.api.MarvelApi
 import com.zensolutions.marvelheroes.data.network.repo.MarvelHeroFetchRepository
+import com.zensolutions.marvelheroes.data.network.retrofit.MarvelTSProvider
 import com.zensolutions.marvelheroes.data.network.retrofit.RetrofitCallHandler
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -20,10 +21,10 @@ class MarvelHeroFetchImpl(
             RetrofitCallHandler.processCall {
                 //TODO JOSE: We need to make a ts and hasher. Preferably with inline
                 service.getHero(
-                    "1",
+                    MarvelTSProvider.setTS(),
                     "Thor",
                     "406402f980734048e838275c3ee9aabb",
-                    "db60ad1dd91f9882dede231cb64f0776"
+                    MarvelTSProvider.setPin()
                 )
             }
         }
